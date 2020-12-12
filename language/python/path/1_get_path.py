c000ca26946e88f7e3acc2765310d01fa38c2e3a
choisungwook <kgg1959@navaer.com>
2020-12-12 19:00:09 +0900

    Add 절대경로

--- language/python/path/1_get_path.py
+++ language/python/path/1_get_path.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 from os import getcwd
+from os.path import abspath
 
 cwd = getcwd()
 path_object = Path(cwd)
 
 # diff getcwd vs Path(getcwd())
+print("str, Path type")
 print(type(str))
 print(type(path_object))
 
 # print parent directory
+print("parent directory")
 print(path_object.parent)
+
+# print abspath
+print("current abs dirpath")
+print(abspath("."))
