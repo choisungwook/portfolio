a229424b640814045fcd473c56d778d34b7089e8
choisungwook <kgg1959@navaer.com>
2020-12-19 15:07:46 +0900

    Change subenet az설정

--- terrafom/4_Vpc_with_Subent/create_vpc_subent.tf
+++ terrafom/4_Vpc_with_Subent/create_vpc_subent.tf
@@ -11,11 +11,24 @@ resource "aws_vpc" "terraform-vpc1" {
   }
 }
 
+# az: ap-northeast-2a
 resource "aws_subnet" "terraform-subent1" {
   vpc_id     = aws_vpc.terraform-vpc1.id
   cidr_block = "172.20.1.0/24"
+  availability_zone = "ap-northeast-2a"
 
   tags = {
     Name = "tf-subnet1"
+  }
+}
+
+# az: ap-northeast-2b
+resource "aws_subnet" "terraform-subent2" {
+  vpc_id     = aws_vpc.terraform-vpc1.id
+  cidr_block = "172.20.2.0/24"
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name = "tf-subnet2"
   }
 }
