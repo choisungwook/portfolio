e9a3960220a1bfe1cebec4c1dca3b141f97ed865
choisungwook <kgg1959@naver.com>
2021-01-27 00:06:19 +0900

    feats: install dcos scripts

---
+++ language/javascript/destructuring/demo1.js
@@ -0,0 +1,7 @@
+//  배열 비구조화
+
+const arr = [1, 2];
+// a=1, b=2;
+const [a, b] = arr;
+
+console.log(arr);
---
+++ language/javascript/destructuring/demo2.js
@@ -0,0 +1,6 @@
+// 객체 비구조화
+
+const obj = {name:'john', age:10};
+const {age, name} = obj;
+
+console.log({age, name});
