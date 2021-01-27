644825dc2e94ac840bda4351dfd1e7f52861cdc1
choisungwook <kgg1959@naver.com>
2021-01-28 08:18:35 +0900

    feats: react props

---
+++ React/props/demo1/Component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyCompnonet = props => {
+    return <div> 안녕하세요. 제 이름은 {props.name}입니다.</div>
+};
+
+export default MyCompnonet;
---
+++ React/props/demo1/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MyCompnonet from './MyComponent';
+
+const App = () => {
+  return <MyCompnonet name="React">리액트</MyCompnonet>
+};
+
+export default App;
