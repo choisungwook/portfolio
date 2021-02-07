84fa389e82f50f9e572e73b9fa71ccc55fb4c58f
choisungwook <kgg1959@naver.com>
2021-02-08 07:41:17 +0900

    feats: Add copying inventory.ini

--- kubernetes/kubespray/bootstrap.sh
+++ kubernetes/kubespray/bootstrap.sh
@@ -24,6 +24,7 @@ yum install git net-tools wget -y
 
 # clone kubespray project
 git clone https://github.com/kubernetes-sigs/kubespray.git
+cp -rfp kubespray/inventory/sample kubespray/inventory/mycluster
 
 # install python package
 sudo pip3 install -r kubespray/requirements.txt
