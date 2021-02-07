093287ac919575eb7e0a993c1c26ac4f6815629d
choisungwook <kgg1959@naver.com>
2021-02-08 08:16:34 +0900

    feats: Add Exception

--- aws/lambda/s3_readfile.py
+++ aws/lambda/s3_readfile.py
@@ -1,20 +1,28 @@
 import boto3
+import botocore
 
 bucketname = 'boannews'
 keyname = 'raw_news/20210206_news.txt'
+unExisting_keyname = "abc.txt"
 
 def test1():
-    s3 = boto3.resource('s3')
-    obj = s3.Object(bucketname, keyname)
-    body = obj.get()['Body'].read().decode()
+    try:
+        s3 = boto3.resource('s3')
+        obj = s3.Object(bucketname, unExisting_keyname)
+        body = obj.get()['Body'].read().decode()
 
-    print(body)
+        print(body)
+    except s3.meta.client.exceptions.NoSuchKey:
+        raise Exception ("Readfile From s3 is failed: {}/{}".format(bucketname, unExisting_keyname))
 
 def test2():
-    s3 = boto3.client('s3')
-    data = s3.get_object(Bucket=bucketname, Key=keyname)
-    contents = data['Body'].read().decode()
-    print(contents)
+    try:
+        s3 = boto3.client('s3')
+        data = s3.get_object(Bucket=bucketname, Key=unExisting_keyname)
+        contents = data['Body'].read().decode()
+        print(contents)
+    except Exception as e:
+        raise "Readfile From s3 is failed: {}".format(e)
 
 test1()
 test2()
