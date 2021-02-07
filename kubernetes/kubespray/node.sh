0f14f72392c1a32856e4350bbc46ad7d6dcf44a9
choisungwook <kgg1959@naver.com>
2021-02-07 18:13:27 +0900

    feats: Add configuration

--- kubernetes/kubespray/node.sh
+++ kubernetes/kubespray/node.sh
@@ -3,7 +3,15 @@
 # change timezone
 timedatectl set-timezone Asia/Seoul
 
-# permiet remote root login
+# disable firealld
+systemctl disable firewalld
+
+# IP forwarding
+modprobe br_netfilter
+sh -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables"
+sh -c "echo '1' > /proc/sys/net/ipv4/ip_forward"
+
+# permit remote root login
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i -e 's/#PermitRootLogin yes/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart sshd
