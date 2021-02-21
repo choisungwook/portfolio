cef90f7ed65549501e6e15cbd1a0e5437639f0ea
choisungwook <kgg1959@naver.com>
2021-02-21 18:12:48 +0900

    feats: fix install linux packages

--- Dcos/manual/jenkins/configuration.sh
+++ Dcos/manual/jenkins/configuration.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 # install docker
-apt install \
+apt install -y --no-install-recommends \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-apt update && apt install docker-ce docker-ce-cli containerd.io
+apt update && apt install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker && systemctl enable docker
 
 # install linux packages
-apt install maven \
-            openjdk-8-jdk \
-            nodejs \
-            npm
+apt install -y \
+    maven \
+    openjdk-8-jdk \
+    nodejs \
+    npm
