c271a4c90186eecfa2af46d88a5d1b1f9ea43365
choisungwook <kgg1959@naver.com>
2021-02-21 18:46:39 +0900

    feats: fix repository url

--- Dcos/manual/jenkins/configuration.sh
+++ Dcos/manual/jenkins/configuration.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 # install docker
-apt update && apt install -y \
+apt update
+apt install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg| apt-key add -
 
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
-apt update && apt install -y docker-ce docker-ce-cli containerd.io
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker && systemctl enable docker
 
 # install linux packages
