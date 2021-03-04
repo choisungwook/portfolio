4fa1c577671e3da70c9bbecdc9c3bf4f93602759
choisungwook <kgg1959@naver.com>
2021-03-04 23:59:45 +0900

    feats: Change baseImage

--- docker/jenkins-dood/configuration.sh
+++ docker/jenkins-dood/configuration.sh
@@ -1,33 +1,20 @@
 #!/bin/sh
 
 # install docker
-apt update
-apt install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
 curl -fsSL https://download.docker.com/linux/debian/gpg| apt-key add -
+yum install docker-ce docker-ce-cli containerd.io -y
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker jenkins
 chown root:jenkins /var/run/docker.sock
 
-# install jdk8
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
-add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-apt update
-apt install -y adoptopenjdk-8-hotspot
-
-apt install -y \
+# install other packages
+yum install -y java-1.8.0-openjdk
+yum install -y 
+yum install -y \
     maven \
     nodejs \
     npm
