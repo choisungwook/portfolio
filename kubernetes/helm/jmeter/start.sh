d63b522797520639504c7d442a1b12e49b096457
choisungwook <kgg1959@naver.com>
2021-03-28 21:48:59 +0900

    jmeter 실행 스크립트 추가

---
+++ kubernetes/helm/jmeter/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+namespace="jmeter"
+master_pod=$(kubectl get pods -n ${namespace} -ljmeter_role=master -o jsonpath='{.items[*].metadata.name}')
+
+jmx="$1"
+if [ ! -f "$jmx" ];
+then
+    echo "Please check that jmx is exist"
+    exit
+fi
+
+kubectl cp "$jmx" -n $namespace "$master_pod:/$jmx"
