34fef5ccce81b6e4499c12a02dfe083ca42b36af
choisungwook <kgg1959@naver.com>
2021-03-30 18:43:17 +0900

    jmeter helm 업데이트

--- kubernetes/helm/jmeter/start.sh
+++ kubernetes/helm/jmeter/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 namespace="jmeter"
 master_pod=$(kubectl get pods -n ${namespace} -ljmeter_role=master -o jsonpath='{.items[*].metadata.name}')
-
+servers=$(kubectl get pods -n jmeter -ljmeter_role=server -o jsonpath='{.items[*].status.podIP}' | tr ' ' ',')
 jmx="$1"
 if [ ! -f "$jmx" ];
 then
@@ -12,4 +12,4 @@ fi
 kubectl cp "$jmx" -n $namespace "$master_pod:/$jmx"
 
 # execute script.jmx
-kubectl exec -ti -n $namespace $master_pod -- /bin/bash /load_test "/$jmx"
+kubectl exec -ti -n $namespace $master_pod -- jmeter -n -t /$jmx -Dserver.rmi.ssl.disable=true -R $servers
