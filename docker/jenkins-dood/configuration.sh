e4e9b6ac6ec631017edfa55dd89c754b0c9e2561
choisungwook <kgg1959@naver.com>
2021-04-22 20:57:25 +0900

    jenkins nvm

--- docker/jenkins-dood/configuration.sh
+++ docker/jenkins-dood/configuration.sh
@@ -33,7 +33,9 @@ apt install -y nodejs
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source /root/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 10.16.3
 npm install -g yarn@1.19.2
 
