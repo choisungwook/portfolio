a34ac3ba2f22940ba4d6d339b063c6891874f83a
choisungwook <kgg1959@naver.com>
2021-05-13 22:22:00 +0900

    SRM Project: 로거 패키징

--- SRM_project/logger/log.py
+++ SRM_project/logger/log.py
@@ -2,15 +2,12 @@ import yaml
 import logging.config
 import logging
 
-try:
-    with open('config.yaml', 'r') as f:
-        config = yaml.safe_load(f)
+with open('logger/config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
-    # reference: https://stackoverflow.com/questions/49012123/python3-logging-yaml-configuration
-    logging.config.dictConfig(config)
-    log = logging.getLogger('simple')
-except Exception as e:
-    print("[-] Error: import logging yaml is failed")
+# reference: https://stackoverflow.com/questions/49012123/python3-logging-yaml-configuration
+logging.config.dictConfig(config)
+log = logging.getLogger('simple')
 
 # example
 # log.debug("do you see me?")
