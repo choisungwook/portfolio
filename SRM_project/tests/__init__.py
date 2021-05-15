e0d20a398280492421a95f430a41701639e29879
choisungwook <kgg1959@naver.com>
2021-05-15 21:41:23 +0900

    SRM Project: gitlab 유저조회 테스트 추가

---
+++ SRM_project/tests/__init__.py
@@ -0,0 +1,5 @@
+
+import os
+from pathlib import Path
+
+os.chdir(Path(os.getcwd()).parent)
---
+++ SRM_project/tests/test_get_gitlabusers.py
@@ -0,0 +1,14 @@
+from apis import gitlab
+from flask import helpers
+import pytest
+from app import app
+from apis.gitlab.service import GitlabImpl
+
+def test_get_gitlabusers():
+    '''
+        gitlab 계정조회하는 함수실행 테스트
+    '''
+    gitlabAPI = GitlabImpl()
+    response = gitlabAPI.getUsers()
+
+    print(response)
