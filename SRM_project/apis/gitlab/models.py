4e33b3b5be8682b6bf04b5866da5729c9dfa9ac8
choisungwook <kgg1959@naver.com>
2021-05-15 21:42:05 +0900

    SRM Project: gitlab 유저조회api, html페이지 추가

---
+++ SRM_project/apis/gitlab/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from db.db import db
+
+class GitlabProject(db.Models):
+    '''
+        Gialb project를 관리
+    '''
+    __table__name = 'Gitlabproject'
+    pass
---
+++ SRM_project/apis/gitlab/service.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+from abc import *
+import requests
+import json
+from urllib.parse import urljoin
+
+from requests.api import head
+from logger.log import log
+from pathlib import Path
+import os
+
+class AbstractGitlab(metaclass=ABCMeta):
+    '''
+        gitlab서비스클래스 추상화
+    '''
+
+    # gitlab API를 사용하기 위한 accesstoekn 설정
+    accesstoken = "gZVSzZMgzMnzkkKiwj3T" # this is for sample
+    gitlabDomain = "https://gitlab.choilab.com" # this is for sample
+    gitlabAPIVersion = "/api/v4/"
+    gitlabURI = urljoin(gitlabDomain, gitlabAPIVersion)
+    selfSignedcertPath = os.path.join(Path(os.getcwd()).parent.parent, 'gitlab_test.pem.cer')
+
+    @abstractmethod
+    def createUser(self):
+        '''
+            gitlab유저 생성
+        '''
+        pass
+    
+    @abstractmethod
+    def createProject(self):
+        '''
+            프로젝트 생성
+        '''
+        pass
+
+    @abstractmethod
+    def getUsers(self):
+        '''
+            gitlab 계정목록 조회
+        '''
+        pass
+
+class GitlabImpl(AbstractGitlab):
+    def createProject(self):
+        '''
+            프로젝트 생성
+        '''
+        return super().createProject()
+
+    def createUser(self):
+        '''
+            gitlab유저 생성
+        '''
+        data = {
+
+        }
+
+        return super().createUser()
+
+    def getUsers(self):
+        '''
+            gitlab 계정목록 조회
+        '''
+        
+        response = {
+            'status': False,
+            'data': []
+        }
+
+        try:
+            URI = urljoin(self.gitlabURI, "users")
+            headers = {"Authorization": "Bearer {}".format(self.accesstoken)}
+            api_response = requests.get(URI, headers=headers)
+            
+            response['status'] = api_response.ok
+
+            if not response['status']:
+                log.debug('[Error 301] 계정목록 조회실패. 응답코드: {}'.format(response['status']))
+            else:
+                log.debug("[OK] 계정목록 조회 성공")
+                response['data'] = api_response.json()
+
+        except Exception as e:
+            log.debug('[Error 300] 계정목록 조회실패: {}'.format(e))
+
+        return response
