83c36697577bc3c0fd516f23bf54dd33ff1646f7
choisungwook <kgg1959@naver.com>
2021-05-16 21:34:24 +0900

    클라우데라 vagrant 업데이트

--- vagrant/Cloudera/install.sh
+++ vagrant/Cloudera/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install cloudera
-
+echo "[*] download trial cloudrea version"
 wget https://archive.cloudera.com/cm7/7.1.4/cloudera-manager-installer.bin
 chmod u+x cloudera-manager-installer.bin
-# sudo ./cloudera-manager-installer.bin
+echo "[*] download done"
