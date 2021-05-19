caa7aa58d74b83f139f8fee2d22b519300840b7b
choisungwook <kgg1959@naver.com>
2021-05-19 12:01:45 +0900

    SRM Project: 로그인 요구메세지

--- SRM_project/login/loginmanager.py
+++ SRM_project/login/loginmanager.py
@@ -1,4 +1,5 @@
 from flask_login import LoginManager
 
 login_manager = LoginManager()
-login_manager.login_view = '/api/v1/auth/signup'
+login_manager.login_view = '/api/v1/auth/signin'
+login_manager.login_message = "Please login"
