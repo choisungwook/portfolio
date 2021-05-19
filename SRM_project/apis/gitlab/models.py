0d9fd8ddff420314b74feee3e58399652d7080ab
choisungwook <kgg1959@naver.com>
2021-05-19 17:15:51 +0900

    SRM Project: 테이블 이름과 필드 수정
    gitlab group과 service project를 구분하기 위한 필드 수정

--- SRM_project/apis/gitlab/models.py
+++ SRM_project/apis/gitlab/models.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from db.db import db
 
-class GitlabProject(db.Model):
+class ServiceProject(db.Model):
     '''
-        Gialb project를 관리
+        서비스 입장에서는 프로트이지만 Gialb group을 관리
     '''
-    __table__name = 'gitlab_project'
+    __tablename__  = 'service_project'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_project_id = db.relationship('UserGitlabMappingEntity', backref='gitlab_project', lazy=True)
+    user_project_id = db.relationship('UserProjectMappingEntity', backref='service_project', lazy=True)
     project_id = db.Column(db.Integer, nullable=False)
     weburl = db.Column(db.String(200), nullable=False)
 
@@ -16,13 +16,13 @@ class GitlabProject(db.Model):
         self.project_id = project_id
         self.weburl = weburl
 
-class UserGitlabMappingEntity(db.Model):
-    __table__name = 'userprojectmapping'
+class UserProjectMappingEntity(db.Model):
+    __tablename__  = 'userprojectmapping'
     
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('gitlab_project.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('service_project.id'), nullable=False)
 
-    def __init__(self, flaskuser_id, gitlabproject_id):
+    def __init__(self, flaskuser_id, project_id):
         self.user_id = flaskuser_id
-        self.project_id = gitlabproject_id
+        self.project_id = project_id
