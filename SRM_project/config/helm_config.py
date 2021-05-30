f99e17db0c6e0701649f266034b69d4385527e36
choisungwook <kgg1959@naver.com>
2021-05-30 18:53:10 +0900

    SRM Project: helm 저장소 ID 추가

--- SRM_project/config/helm_config.py
+++ SRM_project/config/helm_config.py
@@ -2,10 +2,21 @@ import yaml
 
 def get_springboot_helm_rootId():
     '''
-        리턴: jenkins admin access_token
+        리턴: helm springboot helm project ID
     '''
 
     with open('config/global_config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
     return config['helm']['root_Id']['springboot']
+
+def get_common_helm():
+    '''
+        리턴: helm common project
+    '''
+
+    with open('config/global_config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    return config['helm']['common_helm_groupId']
+    
