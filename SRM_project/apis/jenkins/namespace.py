45759d14636ce3b014dc409ce00f6b417956d3f9
choisungwook <kgg1959@naver.com>
2021-06-15 21:00:42 +0900

    SRM Project: 빌드 요청 url 수정

--- SRM_project/apis/jenkins/namespace.py
+++ SRM_project/apis/jenkins/namespace.py
@@ -1,4 +1,5 @@
 from flask_restx import Resource, Namespace
+from logger.log import log
 
 ns = Namespace('jenkins', version="1.0", description='jenkins controller')
 
@@ -7,3 +8,12 @@ class Index(Resource):
     @ns.doc(response={200: "success"})
     def get(self):
         return "This is a jenknis health check api"
+
+@ns.route("/triggerjob/<string:job_path>")
+class Triggerjob(Resource):
+    @ns.doc(response={200: "success"})
+    def get(self, job_path):
+        jenkins_folder, jenkins_job = job_path.split("and")
+        log.debug(f"jenkins_folder: {jenkins_folder}")
+        log.debug(f"jenkins_job: {jenkins_job}")
+        return "This is a jenknis trigger"
