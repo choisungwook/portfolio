a22211c29a8cba11f10d22f74697e794ecb0476a
choisungwook <kgg1959@naver.com>
2021-06-24 20:37:54 +0900

    SRM Project 이동

--- SRM_project/run.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-# run flask on localhost 
-if __name__=="__main__":
-    app.run(host='127.0.0.1', port=8888, debug=True)
--- SRM_project/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-
-import os
-from pathlib import Path
-
-os.chdir(Path(os.getcwd()).parent)
