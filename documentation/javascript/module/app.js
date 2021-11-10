ed2491a7ca892adb2dfb5f13a892862aeb51c008
choisungwook <kgg1959@naver.com>
2021-11-10 22:26:03 +0900

    자바스크립트 문서 추가

---
+++ documentation/javascript/module/app.js
@@ -0,0 +1,10 @@
+// default export
+// import math from "./exportdefault_calc.js";
+
+// console.log(math.plus(1,2));
+// console.log(math.minus(1,2));
+
+// named export
+import {plus, minus} from "./named_calc.js";
+console.log(plus(1,2));
+console.log(minus(1,2));
