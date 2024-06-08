5f3c5adc5eefb69710b3f5033c44837a3f8bc3e1
choisungwook <kgg1959@naver.com>
2024-06-08 21:38:51 +0900

    KAN-176 Add django apps

--- django/helloworld/djangoExample/djangoExample/urls.py
+++ django/helloworld/djangoExample/djangoExample/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
