36f2f5990b43f850a3ead9768b053a86cf5484a5
choisungwook <kgg1959@naver.com>
2024-08-25 19:53:06 +0900

    KAN-209 Create memo django app Dockerfile

--- computer_science/db_migration_with_aws/application/memo_project/memo_project/settings.py
+++ computer_science/db_migration_with_aws/application/memo_project/memo_project/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
