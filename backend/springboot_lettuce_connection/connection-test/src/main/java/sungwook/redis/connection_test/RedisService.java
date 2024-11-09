e55eb308a64eedc6e672a8351bb2a86b4d7b9648
choisungwook <kgg1959@naver.com>
2024-11-10 00:39:08 +0900

    KAN-222 Add debug log and actuator

--- backend/springboot_lettuce_connection/connection-test/src/main/java/sungwook/redis/connection_test/RedisService.java
+++ backend/springboot_lettuce_connection/connection-test/src/main/java/sungwook/redis/connection_test/RedisService.java
@@ -1,6 +1,10 @@
 package sungwook.redis.connection_test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.ClusterInfo;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.RedisClusterNode;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +13,9 @@ public class RedisService {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     public void saveValue(String key, Object value) {
         redisTemplate.opsForValue().set(key, value);
@@ -24,5 +31,19 @@ public class RedisService {
 
     public boolean exists(String key) {
         return redisTemplate.hasKey(key);
+    }
+
+    public ClusterInfo getClusterTopology() {
+        RedisClusterConnection clusterConnection = connectionFactory.getClusterConnection();
+        ClusterInfo clusterInfo = clusterConnection.clusterGetClusterInfo();
+
+        return clusterInfo;
+    }
+
+    public Iterable<RedisClusterNode> getClusterNodes() {
+        RedisClusterConnection clusterConnection = connectionFactory.getClusterConnection();
+        Iterable<RedisClusterNode> redisClusterNodes = clusterConnection.clusterGetNodes();
+
+        return redisClusterNodes;
     }
 }
