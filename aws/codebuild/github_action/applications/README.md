d71b8dc03d2bc0a8320c1e4cc56c5f834d62db6b
choisungwook <9348148+choisungwook@users.noreply.github.com>
2025-10-18 21:02:02 +0900

    Add nexus java use case #251 (#253)

---
+++ aws/codebuild/github_action/applications/README.md
@@ -0,0 +1,8 @@
+# 개요
+
+* codebuild github action에서 자바 라이브러리와 빌드로 사용할 애플리케이션입니다.
+
+## 목차
+
+* [java 라이브러리](./java_modules/)
+* [springboot app](./springboot_app/)
---
+++ aws/codebuild/github_action/applications/java_modules/welcome-lib/src/main/java/com/example/welcome/WelcomeMessage.java
@@ -0,0 +1,11 @@
+package com.example.welcome;
+
+public class WelcomeMessage {
+  public static void printWelcome() {
+    System.out.println("Hello. Welcome");
+  }
+
+  public static String getWelcome() {
+    return "Hello. Welcome";
+  }
+}
